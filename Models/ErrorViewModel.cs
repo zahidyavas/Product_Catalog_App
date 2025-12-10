88d42249d2ca6d0ca2836c2736af385ea657d7e9
zahidyavas <142148911+zahidyavas@users.noreply.github.com>
2025-12-10 18:27:59 +0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Product_Catalog_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
