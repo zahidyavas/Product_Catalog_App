8561bfd43413170314b529e9e381a3f72ccc9fa4
zahidyavas <142148911+zahidyavas@users.noreply.github.com>
2025-12-10 20:10:08 +0300

    Product list and Repository was created for the fake database.

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Product_Catalog_App.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿namespace Product_Catalog_App.Models
+{
+    public class Repository
+    {
+        // Fake Database
+
+        private static List<Product> _products = new();
+        public static List<Product> Products
+        {
+            get { return _products; }
+        }
+
+        private static List<Category> _categories = new();
+        public static List<Category> Categories
+        {
+            get { return _categories; }
+        }
+    }
+}
