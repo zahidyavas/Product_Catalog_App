afe040367760dc81a5b87a68a3e3b3f3b1d08599
zahidyavas <142148911+zahidyavas@users.noreply.github.com>
2025-12-12 17:08:08 +0300

    data was created

--- Models/Repository.cs
+++ Models/Repository.cs
@@ -15,5 +15,19 @@
         {
             get { return _categories; }
         }
+
+        static Repository() // Static constructor was created to initialize data
+        {
+            _categories.Add(new Category { CategoryId = 1, Name = "iPhone" });
+            _categories.Add(new Category { CategoryId = 2, Name = "Macbook" });
+
+            _products.Add(new Product { ProductId = 1, CategoryId = 1, Name = "iPhone 15", Image = "iphone_15.jpg", Price = 29999, IsActive = true });
+            _products.Add(new Product { ProductId = 2, CategoryId = 1, Name = "iPhone 16", Image = "iphone_16.jpg", Price = 39999, IsActive = true });
+            _products.Add(new Product { ProductId = 3, CategoryId = 1, Name = "iPhone 17 Air", Image = "iphone_17_air.jpg", Price = 79999, IsActive = true });
+            _products.Add(new Product { ProductId = 4, CategoryId = 1, Name = "iPhone 17 Pro Max", Image = "iphone_17_pro_max.jpg", Price = 149999, IsActive = true });
+            _products.Add(new Product { ProductId = 5, CategoryId = 2, Name = "Macbook Air", Image = "macbook_air.jpg", Price = 89999, IsActive = true });
+            _products.Add(new Product { ProductId = 6, CategoryId = 3, Name = "Macbook Pro", Image = "macbook_pro.jpg", Price = 129999, IsActive = true });
+            _products.Add(new Product { ProductId = 7, CategoryId = 4, Name = "iMac", Image = "imac.jpg", Price = 199999, IsActive = true });
+        }
     }
 }
