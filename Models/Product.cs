ec147811b8d16de3b3950a997ecbb288511c1275
zahidyavas <142148911+zahidyavas@users.noreply.github.com>
2025-12-12 17:12:48 +0300

    Update Display attributes for Product properties

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,17 @@
-﻿namespace Product_Catalog_App.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_Catalog_App.Models
 {
     public class Product
     {
+        [Display(Name = "Ürün Id")]
         public int ProductId { get; set; }
         public int CategoryId { get; set; }
+        [Display(Name = "Ürün Adı")]
         public string? Name { get; set; }
+        [Display(Name = "Görsel")]
         public string? Image { get; set; }
+        [Display(Name = "Fiyat")]
         public decimal Price { get; set; }
         public bool IsActive { get; set; }
     }
